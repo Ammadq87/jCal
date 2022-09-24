eb614be54dff6655dd3077705195abec392500ee
Ammad <ammadq87@my.yorku.ca>
2022-09-24 11:08:23 -0400

    Added time and date validation functions

--- Validation.java
+++ Validation.java
@@ -3,10 +3,41 @@ public class Validation {
     private String command;
     private String commandTypes[] = { "add", "book", "find", "ls", "login" };
     private String commandType;
+    CommandUtil cu = new CommandUtil();
 
     public Validation(String command) {
-        CommandUtil cu = new CommandUtil();
         this.command = cu.SetCommand(command);
+    }
+
+    public boolean validateTime(int start, int end) {
+        boolean arr[] = { (start >= 0 && end >= 0) && (start <= 2400 && end <= 2400), (start <= end),
+                (start % 100) % 15 == 0 && (end % 100) % 15 == 0 };
+
+        // Find case where value is not true
+        for (boolean value : arr) {
+            if (!value)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean validateDateFormat(String date) {
+        if (date == null || date.isEmpty() || date.isBlank()) {
+            return false;
+        }
+
+        int months[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        String dateValues[] = date.split("-");
+        if (Integer.parseInt(dateValues[0]) > months.length)
+            return false;
+        else if (Integer.parseInt(dateValues[0]) != months[Integer.parseInt(dateValues[0]) - 1])
+            return false;
+        else if (Integer.parseInt(dateValues[1]) > days[Integer.parseInt(dateValues[0]) - 1])
+            return false;
+        else if (Integer.parseInt(dateValues[2]) < 2022)
+            return false;
+        return true;
     }
 
     public boolean RunValidation() {
