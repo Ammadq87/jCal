cd640594a7f70349da870405f81eaf75b4f3a755
Ammad <ammadq87@my.yorku.ca>
2022-09-25 23:46:33 -0400

    Fixed GetArgument to return arguments containing more than 1 word

--- CommandUtil.java
+++ CommandUtil.java
@@ -1,17 +1,22 @@
-import java.util.regex.*;
-import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class CommandUtil {
-    private String flags[] = { "-d", "-t", "-s", "-pr", "-delete", "-edit", "-new", "-u", "-p" };
+    private String flags[] = { "-d", "-t", "-s", "-pr", "-delete", "-edit", "-new", "-u", "-p", "-n" };
     private String commands[] = { "add", "find", "ls", "login" };
     String userDataColumns[] = { "name-s", "username-s", "password-s", "uid-i", "lastLoggedIn-s", "JCal-i" };
-    String eventColumns[] = { "name-s", "date-s", "startTime-i", "endTime-i", "status-i", "priority-i", "eventID-i" };
+    String eventColumns[] = { "name-s", "date-s", "startTime-i", "endTime-i", "status-i", "priority-i", "eventID-i",
+            "jCal-i" };
 
+    /**
+     * Cleans up command by removing extra spaces
+     * 
+     * @param command input command from user
+     * @return proper formatted command w/o extra spaces
+     */
     public String SetCommand(String command) {
         if (isStringNullOrEmpty(command))
             return null;
-
         String text[] = command.split(" ");
         String output = "";
         for (int i = 0; i < text.length; i++) {
@@ -22,6 +27,13 @@ public class CommandUtil {
         return output;
     }
 
+    /**
+     * Supposed to remove quotes from input command
+     * ! Not yet completed
+     * 
+     * @param command argument needed to be sanitized
+     * @returns sanitized command
+     */
     public String SanitizeArgument(String command) {
         if (isStringNullOrEmpty(command))
             return null;
@@ -32,14 +44,24 @@ public class CommandUtil {
         String text[] = command.split(" ");
         if (text == null || text.length == 0)
             return null;
+        String argument = "";
         for (int i = 0; i < text.length; i++) {
             if (text[i].equals(flag)) {
-                if (i != text.length - 1)
-                    return SanitizeArgument(text[i + 1]);
-                else
-                    return null;
+                for (int j = i + 1; j < text.length; j++) {
+                    if (text[j].charAt(0) == '\'' && text[j].charAt(text[j].length() - 1) == '\'')
+                        return text[j];
+                    else if (text[j].charAt(0) == '\'')
+                        argument += text[j] + " ";
+                    else if (text[j].charAt(0) != '\'' && text[j].charAt(text[j].length() - 1) != '\'')
+                        argument += text[j] + " ";
+                    else if (text[j].charAt(text[j].length() - 1) == '\'') {
+                        argument += text[j];
+                        return argument;
+                    }
+                }
             }
         }
+
         return null;
     }
 
